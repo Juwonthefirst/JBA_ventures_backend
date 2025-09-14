35707b7f45f78ab76f32d52d2d30e32cf37af8a5
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-09-14 22:07:39 +0100

    final update

--- v1/property/serializers.py
+++ v1/property/serializers.py
@@ -1,30 +1,33 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from v1.property.models import Property as PropertyModel, PropertyMedia
 
 
-class PropertyMediaSerializer(ModelSerializer):
+class PropertyMediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = PropertyMedia
-        fields = "__all__"
+        fields = ["id", "media"]
+        
 
 
-class PropertySerializer(ModelSerializer):
-    extra_files = PropertyMediaSerializer(many=True)
-
+class PropertySerializer(serializers.ModelSerializer):
+    extra_media_upload = serializers.ListField(child=serializers.FileField(), write_only=True)
+    extra_media = PropertyMediaSerializer(many=True, read_only=True)
+    
     class Meta:
         model = PropertyModel
         fields = "__all__"
 
     def create(self, validated_data):
-        extra_files = validated_data.pop("extra_files", [])
+     
+        extra_media = validated_data.pop("extra_media_upload", [])
         property = PropertyModel.objects.create(**validated_data)
-        for file in extra_files:
+        for file in extra_media:
             PropertyMedia.objects.create(property=property, media=file)
         return property
 
     """def update(self, instance, validated_data):
-        instance. = validated_data.pop("extra_files", [])
-        for file in validated_data.extra_files:
+        instance. = validated_data.pop("extra_media", [])
+        for file in validated_data.extra_media:
             PropertyMedia.objects.create(property=property, media=file)
         return instance
 """
