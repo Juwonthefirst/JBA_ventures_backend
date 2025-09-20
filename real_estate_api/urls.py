a475920e11765431007ad93511a401f843c0cdd3
juwonthefirst <ajibolajuwon5@gmail.com>
2025-09-20 07:02:30 +0100

    Added static file url

--- real_estate_api/urls.py
+++ real_estate_api/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/property/", include("v1.property.urls")),
     path("api/v1/auth/", include("v1.authentication.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
