09a321b9ca2cb92b87407e6a5daf4d0ce6aac1d1
juwonthefirst <ajibolajuwon5@gmail.com>
2025-10-03 00:05:37 +0100

    fixed misnamed settings import

--- createsuperuser.py
+++ createsuperuser.py
@@ -1,16 +1,16 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeepMe.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "real_estate_api.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', os.getenv("USERNAME"))
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', os.getenv("EMAIL"))
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', os.getenv("PASSWORD"))
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", os.getenv("USERNAME"))
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", os.getenv("EMAIL"))
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", os.getenv("PASSWORD"))
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
