4bafa4f7ebc32aad1e148b23504a253bbf542b6c
juwonthefirst <ajibolajuwon5@gmail.com>
2025-10-21 19:44:06 +0100

    fix: changed misnamed env variable from Database_url to db_url

--- real_estate_api/settings.py
+++ real_estate_api/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "real_estate_api.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=os.getenv("DB_URL"),
         conn_max_age=600,
     )
 }
